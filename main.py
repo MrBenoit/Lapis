760d8651b1c30b1a4bcda308e4da6798b693e6bc
Herman <111755919+MrBenoit@users.noreply.github.com>
2024-04-11 09:20:53 +0300

    v 1.0.0 bugfix
    ## Updated
    checker.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 from overwritten import LapisBot
 
 load_dotenv()
-locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
+
+# locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
 
 
 def main() -> None:
@@ -54,85 +55,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-# @bot.slash_command(
-#     description="Загрузить модуль",
-#     guild_ids=[
-#         1025853938207047762
-#     ]
-# )
-# @commands.is_owner()
-# async def load(
-#     interaction: disnake.ApplicationCommandInteraction,
-#     directory: str = commands.Param(name="папка", default=None),
-#     file: str = commands.Param(name="файл", default=None),
-# ):
-#     path = f"extensions.{directory}.{file}"
-#
-#     bot.load_extension(name=path)
-#
-#     await interaction.send(
-#         embed=disnake.Embed(
-#             title=f"Загрузка модуля",
-#             description=f"Модуль {path} загружен",
-#             color=EmbedColor.MAIN_COLOR.value,
-#         ),
-#         ephemeral=True,
-#     )
-#     return
-#
-#
-# @bot.slash_command(
-#     description="Отключить модуль",
-#     guild_ids=[
-#         1025853938207047762
-#     ]
-# )
-# @commands.is_owner()
-# async def unload(
-#     interaction: disnake.ApplicationCommandInteraction,
-#     directory: str = commands.Param(name="папка", default=None),
-#     file: str = commands.Param(name="файл", default=None),
-# ):
-#     path = f"extensions.{directory}.{file}"
-#
-#     bot.unload_extension(name=path)
-#     bot.load_extension(name=path)
-#
-#     await interaction.send(
-#         embed=disnake.Embed(
-#             title=f"Отключение модуля",
-#             description=f"Модуль {path} отключен",
-#             color=EmbedColor.MAIN_COLOR.value,
-#         ),
-#         ephemeral=True,
-#     )
-#     return
-#
-#
-# @bot.slash_command(
-#     description="Перезагрузить модуль",
-#     guild_ids=[
-#         1025853938207047762
-#     ]
-# )
-# @commands.is_owner()
-# async def reload(
-#     interaction: disnake.ApplicationCommandInteraction,
-#     directory: str = commands.Param(name="папка", default=None),
-#     file: str = commands.Param(name="файл", default=None),
-# ):
-#     path = f"extensions.{directory}.{file}"
-#
-#     bot.unload_extension(name=path)
-#     bot.load_extension(name=path)
-#
-#     await interaction.send(
-#         embed=disnake.Embed(
-#             title=f"Перезагрузка модуля",
-#             description=f"Модуль {path} перезагружен",
-#             color=EmbedColor.MAIN_COLOR.value,
-#         ),
-#         ephemeral=True,
-#     )
-#     return
